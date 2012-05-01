8e673576393fd73c1c3d6ff4ea213d11f2fb7b56
Yazz Atlas <yazz.atlas@hp.com>
2012-04-30 23:45:03 -0700

    major change but nicer now

--- build-zones.sh
+++ build-zones.sh
@@ -1,35 +1,41 @@
 # Trying to auto generate my settings for all the different zones
 # we have here at HPCS
+AVALABILITY_ZONES="yazz rndb rndc rnde rndd st1 st2"
+# CURRENT_RUBY="${ruby_rvm_0:-${rvm${zone}}}"
+#set -x
 if [ -s "${HOME}/.bash_cloud_secrets" ] ; then
   source "${HOME}/.bash_cloud_secrets"
   rm -f ${HOME}/.bash_openstack_zones
   echo "# This was generated with ~/dotfiles/build-zones.sh" > ${HOME}/.bash_openstack_zones
-  for zone in rndb rndc rnde rndd st1 st2
+  echo "# the private varables are sourced from ~/.bash_cloud_secrets " >> ${HOME}/.bash_openstack_zones
+  echo "# " >> ${HOME}/.bash_openstack_zones
+  for zone in ${AVALABILITY_ZONES}
   do
   cat <<EOF>> ${HOME}/.bash_openstack_zones
   ${zone}-1(){
-    export OS_TENANT_NAME="${tenant_1}"
-    export OS_USERNAME="${username_1}"
-    export OS_PASSWORD="${password_1}"
-    export OS_REGION_NAME="az-1.region-a.geo-1"
-    export OS_AUTH_URL="${os_auth_url_2:-${os_auth_url_0}}"
-    export CLOUD_PROMPT="${Green}${UGreen}${zone}${Color_Off}"
-    CURRENT_RUBY="${ruby_rvm_0:-${rvm_${zone}}}"
-    rvm ${ruby_rvm_0:-rvm_${zone}}
-    CURRENT_PYTHON="${python_virtualenv_0:-${python_${zone}}}"
-    source "${python_virtualenv_0:-${python_${zone}}}"
+    # ${zone}-1 settings 
+    export OS_TENANT_NAME="\$${zone}_tenant"
+    export OS_USERNAME="\$${zone}_username"
+    export OS_PASSWORD="\$${zone}_password"
+    export OS_REGION_NAME="\$${zone}_region_name_1"
+    export OS_AUTH_URL="\$${zone}_auth_url"
+    export CLOUD_PROMPT="\${UGreen}${zone}-1\${Color_Off}"
+    export CURRENT_RUBY="\$${zone}_rvm"
+    rvm \${CURRENT_RUBY}
+    export CURRENT_PYTHON="\$${zone}_python"
+    source \${CURRENT_PYTHON}
   }
   ${zone}-2(){
-    export OS_TENANT_NAME="${tenant_1}"
-    export OS_USERNAME="${username_2}"
-    export OS_PASSWORD="${password_2}"
-    export OS_REGION_NAME="az-2.region-a.geo-1"
-    export OS_AUTH_URL="${os_auth_url_2:-${os_auth_url_0}}"
-    export CLOUD_PROMPT="${Green}${UBlue}${zone}${Color_Off}"
-    CURRENT_RUBY="${ruby_rvm_0:-${rvm${zone}}}"
-    rvm "${ruby_rvm_0:-${rvm_${zone}-2}}"
-    CURRENT_PYTHON="${python_virtualenv_0:-${python_${zone}}}"
-    source "${python_virtualenv_0:-${python_${zone}}}"
+    export OS_TENANT_NAME="\$${zone}_tenant"
+    export OS_USERNAME="\$${zone}_username"
+    export OS_PASSWORD="\$${zone}_password"
+    export OS_REGION_NAME="\$${zone}_region_name_2"
+    export OS_AUTH_URL="\$${zone}_auth_url"
+    export CLOUD_PROMPT="\${UBlue}${zone}-2\${Color_Off}"
+    export CURRENT_RUBY="\$${zone}_rvm"
+    rvm \${CURRENT_RUBY}
+    export CURRENT_PYTHON="\$${zone}_python"
+    source \${CURRENT_PYTHON}
   }
 EOF
   done
