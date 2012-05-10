b90e8aaee4eee493d5d8c78afba89732a29d5337
Yazz Atlas <yazz.atlas@hp.com>
2012-05-09 19:35:56 -0700

    you can pass your one list to the command

--- build-zones.sh
+++ build-zones.sh
@@ -1,17 +1,20 @@
 # Trying to auto generate my settings for all the different zones
 # we have here at HPCS
-AVALABILITY_ZONES="yazz aaron rndb rndc rnde rndd st1 st2"
-# CURRENT_RUBY="${ruby_rvm_0:-${rvm${zone}}}"
-#set -x
-if [ -s "${HOME}/.bash_cloud_secrets" ] ; then
-  source "${HOME}/.bash_cloud_secrets"
-  rm -f ${HOME}/.bash_openstack_zones
-  echo "# This was generated with ~/dotfiles/build-zones.sh" > ${HOME}/.bash_openstack_zones
-  echo "# the private varables are sourced from ~/.bash_cloud_secrets " >> ${HOME}/.bash_openstack_zones
-  echo "# " >> ${HOME}/.bash_openstack_zones
-  for zone in ${AVALABILITY_ZONES}
+AVALABILITY_ZONES="rnda rndb rndc rnde rndd st1 st2"
+#
+if [ -s "${HOME}/.cloud_secrets" ] ; then
+  source "${HOME}/.cloud_secrets"
+  rm -f ${HOME}/.bash_rnd_zones
+  echo "# This was generated with ~/dotfiles/build-zones.sh" > ${HOME}/.bash_rnd_zones
+  echo "# the private varables are sourced from ~/.cloud_secrets " >> ${HOME}/.bash_rnd_zones
+  echo "# " >> ${HOME}/.bash_rnd_zones
+  for zone in $* ${AVALABILITY_ZONES}
   do
-  cat <<EOF>> ${HOME}/.bash_openstack_zones
+  cat <<EOF>> ${HOME}/.bash_rnd_zones
+  ${zone}-knife(){
+    export CURRENT_KNIFE_ENV="\$${zone}_knife"
+    knife block use \${CURRENT_KNIFE_ENV}
+  }
   ${zone}-1(){
     # ${zone}-1 settings 
     export OS_TENANT_NAME="\$${zone}_tenant"
@@ -24,8 +27,7 @@ if [ -s "${HOME}/.bash_cloud_secrets" ] ; then
     rvm \${CURRENT_RUBY}
     export CURRENT_PYTHON="\$${zone}_python"
     source \${CURRENT_PYTHON}
-    export CURRENT_KNIFE_ENV="\$${zone}_knife"
-    alias ${zone}-knife='knife block use \${CURRENT_KNIFE_ENV} --yes'
+    ${zone}-knife 
   }
   ${zone}-2(){
     export OS_TENANT_NAME="\$${zone}_tenant"
@@ -38,11 +40,24 @@ if [ -s "${HOME}/.bash_cloud_secrets" ] ; then
     rvm \${CURRENT_RUBY}
     export CURRENT_PYTHON="\$${zone}_python"
     source \${CURRENT_PYTHON}
-    export CURRENT_KNIFE_ENV="\$${zone}_knife"
-    alias ${zone}-knife='knife block use \${CURRENT_KNIFE_ENV} --yes'
+    ${zone}-knife 
+  }
+  ${zone}-3(){
+    # ${zone}-1 settings 
+    export OS_TENANT_NAME="\$${zone}_tenant"
+    export OS_USERNAME="\$${zone}_username"
+    export OS_PASSWORD="\$${zone}_password"
+    export OS_REGION_NAME="\$${zone}_region_name_3"
+    export OS_AUTH_URL="\$${zone}_auth_url"
+    export CLOUD_PROMPT="\${URed}${zone}-3\${Color_Off}"
+    export CURRENT_RUBY="\$${zone}_rvm"
+    rvm \${CURRENT_RUBY}
+    export CURRENT_PYTHON="\$${zone}_python"
+    source \${CURRENT_PYTHON}
+    ${zone}-knife 
   }
 EOF
   done
 else
-  echo "You need to have a ${HOME}/.bash_cloud_secrets file"
+  echo "You need to have a ${HOME}/.cloud_secrets file"
 fi
