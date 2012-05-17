f33aa44b685584957d29999f051ad4759dab8817
Yazz Atlas <yazz.atlas@hp.com>
2012-05-16 18:42:27 -0700

    modified to but still not great

--- build-zones.sh
+++ build-zones.sh
@@ -8,6 +8,7 @@ if [ -s "${HOME}/.cloud_secrets" ] ; then
   echo "# This was generated with ~/dotfiles/build-zones.sh" > ${HOME}/.bash_rnd_zones
   echo "# the private varables are sourced from ~/.cloud_secrets " >> ${HOME}/.bash_rnd_zones
   echo "# " >> ${HOME}/.bash_rnd_zones
+  echo "source \${HOME}/.cloud_secrets" >> ${HOME}/.bash_rnd_zones
   for zone in $* ${AVALABILITY_ZONES}
   do
   cat <<EOF>> ${HOME}/.bash_rnd_zones
@@ -22,39 +23,61 @@ if [ -s "${HOME}/.cloud_secrets" ] ; then
     export OS_PASSWORD="\$${zone}_password"
     export OS_REGION_NAME="\$${zone}_region_name_1"
     export OS_AUTH_URL="\$${zone}_auth_url"
+    export AWS_ACCESS_KEY_ID="\$${zone}_api_1"
+    export AWS_SECRET_ACCESS_KEY="\$${zone}_api_secret"
+    export AWS_ACCESS_KEY=\$AWS_ACCESS_KEY_ID
+    export AWS_SECRET_KEY=\$AWS_SECRET_ACCESS_KEY
     export CLOUD_PROMPT="\${UGreen}${zone}-1\${Color_Off}"
     export CURRENT_RUBY="\$${zone}_rvm"
     rvm \${CURRENT_RUBY}
     export CURRENT_PYTHON="\$${zone}_python"
     source \${CURRENT_PYTHON}
-    ${zone}-knife 
+    readlink ~/.chef/knife.rb | grep knife-\$${zone}_knife 
+    if [ \$? -eq 1 ]   ; then
+      ${zone}-knife 
+    fi
   }
   ${zone}-2(){
+    # ${zone}-2 settings 
     export OS_TENANT_NAME="\$${zone}_tenant"
     export OS_USERNAME="\$${zone}_username"
     export OS_PASSWORD="\$${zone}_password"
     export OS_REGION_NAME="\$${zone}_region_name_2"
     export OS_AUTH_URL="\$${zone}_auth_url"
+    export AWS_ACCESS_KEY="\$${zone}_api_2"
+    export AWS_SECRET_ACCESS_KEY="\$${zone}_api_secret"
+    export AWS_ACCESS_KEY=\$AWS_ACCESS_KEY_ID
+    export AWS_SECRET_KEY=\$AWS_SECRET_ACCESS_KEY
     export CLOUD_PROMPT="\${UBlue}${zone}-2\${Color_Off}"
     export CURRENT_RUBY="\$${zone}_rvm"
     rvm \${CURRENT_RUBY}
     export CURRENT_PYTHON="\$${zone}_python"
     source \${CURRENT_PYTHON}
-    ${zone}-knife 
+    readlink ~/.chef/knife.rb | grep knife-\$${zone}_knife 
+    if [ \$? -eq 1 ]   ; then
+      ${zone}-knife 
+    fi
   }
   ${zone}-3(){
-    # ${zone}-1 settings 
+    # ${zone}-3 settings 
     export OS_TENANT_NAME="\$${zone}_tenant"
     export OS_USERNAME="\$${zone}_username"
     export OS_PASSWORD="\$${zone}_password"
     export OS_REGION_NAME="\$${zone}_region_name_3"
     export OS_AUTH_URL="\$${zone}_auth_url"
+    export AWS_ACCESS_KEY="\$${zone}_api_3"
+    export AWS_SECRET_ACCESS_KEY="\$${zone}_api_secret"
+    export AWS_ACCESS_KEY=\$AWS_ACCESS_KEY_ID
+    export AWS_SECRET_KEY=\$AWS_SECRET_ACCESS_KEY
     export CLOUD_PROMPT="\${URed}${zone}-3\${Color_Off}"
     export CURRENT_RUBY="\$${zone}_rvm"
     rvm \${CURRENT_RUBY}
     export CURRENT_PYTHON="\$${zone}_python"
     source \${CURRENT_PYTHON}
-    ${zone}-knife 
+    readlink ~/.chef/knife.rb | grep knife-\$${zone}_knife 
+    if [ \$? -eq 1 ]   ; then
+      ${zone}-knife 
+    fi
   }
 EOF
   done
