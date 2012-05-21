7cb472a2c23b45f0917a8ff4eb289bb5982e5523
Yazz Atlas <yazz.atlas@hp.com>
2012-05-21 13:59:43 -0700

    making it handle more Clouds

--- build-zones.sh
+++ build-zones.sh
@@ -1,6 +1,8 @@
 # Trying to auto generate my settings for all the different zones
 # we have here at HPCS
-AVALABILITY_ZONES="rnda rndb rndc rnde rndd st1 st2"
+#AVALABILITY_ZONES="rnda rndb rndc rnde rndd st1 st2"
+AVALABILITY_ZONES=$(grep _username ~/.cloud_secrets |grep -v \"\" | cut -d "_" -f 1)
+
 #
 if [ -s "${HOME}/.cloud_secrets" ] ; then
   source "${HOME}/.cloud_secrets"
@@ -9,75 +11,115 @@ if [ -s "${HOME}/.cloud_secrets" ] ; then
   echo "# the private varables are sourced from ~/.cloud_secrets " >> ${HOME}/.bash_rnd_zones
   echo "# " >> ${HOME}/.bash_rnd_zones
   echo "source \${HOME}/.cloud_secrets" >> ${HOME}/.bash_rnd_zones
+  echo "export NOVA_VERSION=1.1" >> ${HOME}/.bash_rnd_zones
+
+  cat <<EOF>> ${HOME}/.bash_rnd_zones
+  check-knife(){
+    if [ -z "\${1}" ] ; then
+      echo "- No zone passed as parameter-"
+    else
+      knife_block="\$${1}_knife"
+      readlink ~/.chef/knife.rb | grep knife-\${knife_block}
+      if [ \$? -eq 1 ]   ; then
+        \${1}-knife 
+      fi
+    fi
+  }
+EOF
+
   for zone in $* ${AVALABILITY_ZONES}
   do
   cat <<EOF>> ${HOME}/.bash_rnd_zones
+  # ${zone} change knife.rb
   ${zone}-knife(){
     export CURRENT_KNIFE_ENV="\$${zone}_knife"
     knife block use \${CURRENT_KNIFE_ENV}
   }
+  # ${zone}-1 settings 
   ${zone}-1(){
-    # ${zone}-1 settings 
     export OS_TENANT_NAME="\$${zone}_tenant"
     export OS_USERNAME="\$${zone}_username"
     export OS_PASSWORD="\$${zone}_password"
     export OS_REGION_NAME="\$${zone}_region_name_1"
     export OS_AUTH_URL="\$${zone}_auth_url"
+
+    export NOVA_USERNAME="\$${zone}_username"
+    export NOVA_API_KEY="\$${zone}_password"
+    export NOVA_PROJECT_ID="\$${zone}_tenant"
+    export NOVA_URL="\$${zone}_auth_url"
+
+    export HP_ACCOUNT="\$${zone}_tenant_id"
+    export HP_SECRET="\$${zone}_api_secret"
+    export HP_AUTH="\$${zone}_auth_url"
+    export HP_AVL_ZONE="az1"
+
     export AWS_ACCESS_KEY_ID="\$${zone}_api_1"
     export AWS_SECRET_ACCESS_KEY="\$${zone}_api_secret"
     export AWS_ACCESS_KEY=\$AWS_ACCESS_KEY_ID
     export AWS_SECRET_KEY=\$AWS_SECRET_ACCESS_KEY
+
     export CLOUD_PROMPT="\${UGreen}${zone}-1\${Color_Off}"
     export CURRENT_RUBY="\$${zone}_rvm"
-    rvm \${CURRENT_RUBY}
     export CURRENT_PYTHON="\$${zone}_python"
+
+    rvm \${CURRENT_RUBY}
     source \${CURRENT_PYTHON}
-    readlink ~/.chef/knife.rb | grep knife-\$${zone}_knife 
-    if [ \$? -eq 1 ]   ; then
-      ${zone}-knife 
-    fi
+
+    check-knife ${zone}_knife
   }
+  # ${zone}-2 settings 
   ${zone}-2(){
-    # ${zone}-2 settings 
     export OS_TENANT_NAME="\$${zone}_tenant"
     export OS_USERNAME="\$${zone}_username"
     export OS_PASSWORD="\$${zone}_password"
     export OS_REGION_NAME="\$${zone}_region_name_2"
     export OS_AUTH_URL="\$${zone}_auth_url"
+
+    export HP_ACCOUNT="\$${zone}_tenant_id"
+    export HP_SECRET="\$${zone}_api_secret"
+    export HP_AUTH="\$${zone}_auth_url"
+    export HP_AVL_ZONE="az2"
+
     export AWS_ACCESS_KEY="\$${zone}_api_2"
     export AWS_SECRET_ACCESS_KEY="\$${zone}_api_secret"
     export AWS_ACCESS_KEY=\$AWS_ACCESS_KEY_ID
     export AWS_SECRET_KEY=\$AWS_SECRET_ACCESS_KEY
+
     export CLOUD_PROMPT="\${UBlue}${zone}-2\${Color_Off}"
     export CURRENT_RUBY="\$${zone}_rvm"
-    rvm \${CURRENT_RUBY}
     export CURRENT_PYTHON="\$${zone}_python"
+
+    rvm \${CURRENT_RUBY}
     source \${CURRENT_PYTHON}
-    readlink ~/.chef/knife.rb | grep knife-\$${zone}_knife 
-    if [ \$? -eq 1 ]   ; then
-      ${zone}-knife 
-    fi
+
+    check-knife ${zone}_knife
   }
+  # ${zone}-3 settings 
   ${zone}-3(){
-    # ${zone}-3 settings 
     export OS_TENANT_NAME="\$${zone}_tenant"
     export OS_USERNAME="\$${zone}_username"
     export OS_PASSWORD="\$${zone}_password"
     export OS_REGION_NAME="\$${zone}_region_name_3"
     export OS_AUTH_URL="\$${zone}_auth_url"
+
+    export HP_ACCOUNT="\$${zone}_tenant_id"
+    export HP_SECRET="\$${zone}_api_secret"
+    export HP_AUTH="\$${zone}_auth_url"
+    export HP_AVL_ZONE="az3"
+
     export AWS_ACCESS_KEY="\$${zone}_api_3"
     export AWS_SECRET_ACCESS_KEY="\$${zone}_api_secret"
     export AWS_ACCESS_KEY=\$AWS_ACCESS_KEY_ID
     export AWS_SECRET_KEY=\$AWS_SECRET_ACCESS_KEY
+
     export CLOUD_PROMPT="\${URed}${zone}-3\${Color_Off}"
     export CURRENT_RUBY="\$${zone}_rvm"
-    rvm \${CURRENT_RUBY}
     export CURRENT_PYTHON="\$${zone}_python"
+
+    rvm \${CURRENT_RUBY}
     source \${CURRENT_PYTHON}
-    readlink ~/.chef/knife.rb | grep knife-\$${zone}_knife 
-    if [ \$? -eq 1 ]   ; then
-      ${zone}-knife 
-    fi
+
+    check-knife ${zone}_knife
   }
 EOF
   done
